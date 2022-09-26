00631e24c621a1bc94656ffd5c5bd84c9193c897
aadzhaan <33817970+aadzhaan@users.noreply.github.com>
2022-09-27 06:30:51 +0700

    revisi

--- app/Models/TransJual.php
+++ app/Models/TransJual.php
@@ -10,16 +10,11 @@ class TransJual extends Model
     use HasFactory;
     protected $table = 'htrans_jual';
     protected $primaryKey = 'no_trans_jual';
-    protected $fillable = ['no_trans_jual', 'kode_promo', 'kode_jasa', 'kode_bayar', 'no_booking', 'tgl_trans_jual', 'total_jual', 'bayar_jual', 'kembali_jual', 'tgl_max_garansi'];
+    protected $fillable = ['no_trans_jual', 'promo_id', 'jasa_id', 'bayar_id', 'booking_id', 'tgl_trans_jual', 'total_jual', 'bayar_jual', 'kembali_jual', 'tgl_max_garansi'];
 
     public function Promo()
     {
-        return $this->belongsTo(Promo::class, 'kode_promo');
-    }
-
-    public function Jasa()
-    {
-        return $this->belongsTo(Jasa::class, 'kode_jasa');
+        return $this->belongsTo(Promo::class);
     }
 
     public function Booking()
